397f60c75a67be8ed8ee8e91ba1680e33bf7e753
Taylor Nilsen <taylormnilsen@gmail.com>
2025-09-22 17:35:49 -0700

    app creation

---
+++ BruinBite/BruinBite/BruinBiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BruinBiteApp.swift
+//  BruinBite
+//
+//  Created by Taylor Nilsen on 9/22/25.
+//
+
+import SwiftUI
+
+@main
+struct BruinBiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
